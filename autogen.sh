7f04920a60e6a33d3e63e377f6f9826c1a18a22a
wyatt8740 <wyattfward@gmail.com>
2023-10-04 16:44:36 -0400

    fixed autogen.sh for new gettext

--- autogen.sh
+++ autogen.sh
@@ -31,8 +31,8 @@ echo searching for GNU gettext intl directory...
 dirs="/usr/share /usr/local/share /opt/share /usr /usr/local /opt /usr/gnu/share /opt/local /opt/local/share"
 found=0
 for try in $dirs; do
-	echo -n " -> $try/gettext/intl... "
-	if test -d $try/gettext/intl; then
+	echo -n " -> $try/gettext/po... "
+	if test -d $try/gettext/po; then
 		echo found it
 		found=1
 		break
@@ -40,33 +40,32 @@ for try in $dirs; do
 	echo no
 done
 if test "$found" != 1; then
-	echo ERROR: Cannot find gettext/intl directory.
+	echo ERROR: Cannot find gettext/po directory.
 	echo ERROR: Install GNU gettext in /usr or /usr/local prefix.
 	exit 7
 fi;
 
-echo copying gettext intl files...
+echo copying gettext files...
 if test -f $try/gettext/mkinstalldirs; then
 	ln -s $try/gettext/mkinstalldirs mkinstalldirs
 fi
-intldir="$try/gettext/intl"
-if test ! -d intl; then
-	mkdir intl
-fi
+#intldir="$try/gettext/intl"
+#if test ! -d intl; then
+#	mkdir intl
+#fi
 olddir=`pwd`
-cd $intldir
-for file in *; do
-	if test $file != COPYING.LIB-2.0 && test $file != COPYING.LIB-2.1; then
-		rm -f $olddir/intl/$file
-		cp $intldir/$file $olddir/intl/
-	fi
-done
-cp -f $try/gettext/po/Makefile.in.in $olddir/po/
-cd $olddir
-if test -f intl/plural.c; then
-	sleep 2
-	touch intl/plural.c
-fi
+#cd $intldir
+#for file in *; do
+#	if test $file != COPYING.LIB-2.0 && test $file != COPYING.LIB-2.1; then
+#		rm -f $olddir/intl/$file
+#		cp $intldir/$file $olddir/intl/
+#	fi
+#done
+cp -f $try/gettext/po/Makefile.in.in ./po/
+#if test -f intl/plural.c; then
+#	sleep 2
+#	touch intl/plural.c
+#fi
 
 # ------ END GETTEXT ------
 
